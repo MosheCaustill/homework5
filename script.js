a09e76742af3f580f3f2825b31da87277ceb00a4
MosheCaustill <111441912+MosheCaustill@users.noreply.github.com>
2022-09-08 18:23:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let chessBoard=[];
-let colors=[`black`,`white`];
-let chessrow=[];
-for (let i=0;i<8;i++) {
-    let chessrow=[];
-    for (let j=0;j<8;j++) {
-        chessrow[j]=colors[(i+j)%2];
-    }
-    chessBoard[i]=chessrow;
+let chessBoard = [];
+let colors = [`black`, `white`];
+let chessrow = [];
+for (let i = 0; i < 8; i++) {
+  chessrow = [];
+  for (let j = 0; j < 8; j++) {
+    chessrow[j] = colors[(i + j) % 2];
+  }
+  chessBoard[i] = chessrow;
 }
 console.log(chessBoard);
