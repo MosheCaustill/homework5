dc0aca2b4bb4da347fdca4b5e0e81b544b7e852a
MosheCaustill <111441912+MosheCaustill@users.noreply.github.com>
2022-09-11 00:15:53 +0300

    homework5

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-let color1 = prompt('choose a color');
+let color1 = prompt("choose a color");
 let color2 = prompt(`chosse a different color`);
 let chessBoard = [];
 let colors = [color1, color2];
-let chessrow = [];
+let cellid;
 for (let i = 0; i < 8; i++) {
-  chessrow = [];
+  chessBoard[i] = [];
   for (let j = 0; j < 8; j++) {
-    chessrow[j] = colors[(i + j) % 2];
+    chessBoard[i][j] = colors[(i + j) % 2];
+    cellid = i.toString() + j.toString();
+    var cell = document.getElementById(cellid);
+    cell.style.background = colors[(i + j) % 2];
   }
-  chessBoard[i] = chessrow;
 }
 console.log(chessBoard);
-for (let k = 0; k < 8; k++) {
-  let line = "board" + String(k);
-  console.log(line);
-  const rectWrapDiv = document.getElementById(line).children;
-  console.log(rectWrapDiv);
-  for (const prop of rectWrapDiv) {
-    prop.classList.add("rectangle");
-  }
-  for (let i = 0; i < rectWrapDiv.length; i++) {
-    console.log(rectWrapDiv.length);
-    rectWrapDiv[i].style.background = colors[(i+k) % 2];
-  }
-}
